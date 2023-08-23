397aa5c626f803af96c9baaf70dc08ce11477df3
Leniors <mokualeeroy@gmail.com>
2023-08-23 13:13:58 -0700

    push and pall

---
+++ monty.c
@@ -0,0 +1,58 @@
+#include "monty.h"
+
+/**
+ * main - main function
+ * Return: zero on success
+ */
+int main(int argc, char **argv)
+{
+	stack_t *stack = NULL;
+	stack_t *temp = NULL;
+	FILE *file = fopen(argv[1], "r");
+	char line[100];
+	int i;
+	char opcode[10];
+	unsigned int line_number;
+	int num_instructions;
+	instruction_t instructions[] = {
+		{"push", push},
+		{"pall", pall}
+	};
+
+	sscanf(line, "%s %u", opcode, &line_number);
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	if (!file)
+	{
+		fprintf(stderr, "Can't open file %s", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	while (fgets(line, sizeof(line), file))
+	{
+		sscanf(line, "%s %u", opcode, &line_number);
+		num_instructions = sizeof(instructions) / sizeof(instructions[0]);
+		for (i = 0; i < num_instructions; i++)
+		{
+			if (strcmp(opcode, instructions[i].opcode) == 0)
+			{
+				instructions[i].f(&stack, line_number);
+				break;
+			}
+		}
+	}
+
+	fclose(file);
+
+	while (stack)
+	{
+		temp = stack;
+		stack = stack->next;
+		free(temp);
+	}
+
+	return (0);
+}
---
+++ push.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * push - push element to a stack
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new_node = malloc(sizeof(stack_t));
+
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new_node->n = line_number;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack)
+	{
+		(*stack)->prev = new_node;
+	}
+	*stack = new_node;
+}
