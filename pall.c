cfddbabc719fd61203b4b18b995d9ab8f48cc615
Leniors <mokualeeroy@gmail.com>
2023-08-24 06:06:11 -0700

    push and pall

--- pall.c
+++ pall.c
@@ -8,6 +8,10 @@ void pall(stack_t **stack, unsigned int line_number)
 	stack_t *current = *stack;
 	(void)line_number;
 
+	if (current == NULL)
+	{
+		return;
+	}
 	while (current)
 	{
 		fprintf(stdout ,"%d\n", current->n);
