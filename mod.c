db95826099dcd30804dc04e7cbf53228a2fbe64f
Leniors <mokualeeroy@gmail.com>
2023-08-24 08:39:54 -0700

    mod

---
+++ mod.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+
+/**
+ * add - adds up top elements in a stack
+ */
+void mod(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+	stack_t *current = *stack;
+	int num_nodes = 0;
+
+	while (current)
+	{
+		num_nodes++;
+		current = current->prev;
+	}
+
+	if (num_nodes < 2)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	*stack = (*stack)->prev;
+	if ((*stack)->n == 0 || temp->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->n = (*stack)->n % temp->n;
+	free(temp);
+}
