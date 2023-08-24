eeead5cb8202586b474af61aaf5947b6555ab2a8
Leniors <mokualeeroy@gmail.com>
2023-08-24 09:51:16 -0700

    putcha

--- pchar.c
+++ pchar.c
@@ -7,11 +7,13 @@ void pchar(stack_t **stack, unsigned int line_number)
 {
 	if (stack == NULL || *stack == NULL)
 	{
+		fclose(global.file);
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	if ((*stack)->n < 64 || (*stack)->n > 127)
 	{
+		fclose(global.file);
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 		exit(EXIT_FAILURE);
 	}
