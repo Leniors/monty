228cb2ec7fda9558710ebffc78a3f71eb6cbc2fb
Leniors <mokualeeroy@gmail.com>
2023-08-24 04:52:08 -0700

    push and pall

--- is_integer.c
+++ is_integer.c
@@ -5,21 +5,23 @@
  */
 bool is_integer(const char *str) {
 	int i = 0;
-
-    if (str == NULL || strlen(str) == 0) {
-        return false;
-    }
-
-    if (str[i] == '-' || str[i] == '+') {
-        i++;
-    }
-
-    while (str[i] != '\0') {
-        if (!isdigit(str[i])) {
-            return false;
-        }
-        i++;
-    }
-
-    return true;
+	
+	if (str == NULL || strlen(str) == 0)
+	{
+		return false;
+	}
+	
+	if (str[i] == '-' || str[i] == '+')
+	{
+		i++;
+	}
+	while (str[i] != '\0')
+	{
+		if (!isdigit(str[i]))
+		{
+			return false;
+		}
+		i++;
+	}
+	return true;
 }
