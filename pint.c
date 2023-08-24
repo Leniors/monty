68cec59725cb298ae639d8ca54d58cfb4a57bbda
Leniors <mokualeeroy@gmail.com>
2023-08-24 06:16:52 -0700

    pint

--- pint.c
+++ pint.c
@@ -6,5 +6,18 @@
 void pint(stack_t **stack, unsigned int line_number)
 {
 	(void)line_number;
+	stack_t *current = *stack;
+	int num_nodes = 0;
+
+	while (current)
+	{
+		num_nodes++;
+		current = current->prev;
+	}
+	if (num_nodes == 0)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty", line_number);
+		exit(EXIT_FAILURE);
+	}
 	fprintf(stdout, "%d\n", (*stack)->n);
 }
