29df8f942f2aca7c13973c534cbe1c6535117df9
Leniors <mokualeeroy@gmail.com>
2023-08-24 06:51:34 -0700

    swap

--- swap.c
+++ swap.c
@@ -6,8 +6,19 @@
 void swap(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
-	(void)line_number;
+	stack_t *current = *stack;
+	int num_nodes = 0;
 
+	while (current)
+	{
+		num_nodes++;
+		current = current->prev;
+	}
+	if (num_nodes < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 	temp = (*stack)->prev;
 	(*stack)->prev = temp->prev;
 	temp->prev->next = *stack;
